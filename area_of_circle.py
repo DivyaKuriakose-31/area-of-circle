25291957990952dbda444c2e4c930fccb3d9d860
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-10-11 15:37:00 +0530

    Update area_of_circle.py

--- area_of_circle.py
+++ area_of_circle.py
@@ -1,4 +1,4 @@
 from math import pi
 radius=float(input("Enter the radius of the circle: "))
 area=pi *radius*radius
-print("The area of the circle is:" + str(area))
+print("The area of the circle is:",area)
